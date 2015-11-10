bb4a01331bf2ef773f05f70b3614e325132825ab
gittrue <maxxl2012@gmail.com>
2015-11-10 12:29:12 +0200

    FirstCommitSite StroiTrend [Created Shapka90%]
    Signed-off-by: gittrue <maxxl2012@gmail.com>

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/OpenCart/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/OpenCart/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OpenCart/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OpenCart/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/OpenCart/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/OpenCart/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/OpenCart/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OpenCart/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/OpenCart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/OpenCart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/OpenCart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/OpenCart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/OpenCart/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/OpenCart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OpenCart/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OpenCart/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/OpenCart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/OpenCart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/OpenCart/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OpenCart/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/OpenCart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/OpenCart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/OpenCart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/OpenCart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/OpenCart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
