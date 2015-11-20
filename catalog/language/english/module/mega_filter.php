243df1a062aa616fcf78fea1ccca99e701a80b37
gittrue <maxxl2012@gmail.com>
2015-11-20 13:37:50 +0200

    preinstall product

--- catalog/language/english/module/mega_filter.php
+++ catalog/language/english/module/mega_filter.php
@@ -1,12 +1,12 @@
 <?php
 $_['heading_title'] = 'Mega Filter';
-$_['name_price'] = 'Price';
+$_['name_price'] = 'Цена';
 $_['name_manufacturers'] = 'Manufacturers';
-$_['name_rating'] = 'Rating';
-$_['name_search'] = 'Search';
+$_['name_rating'] = 'Рейтинг';
+$_['name_search'] = 'Поиск';
 $_['name_stock_status'] = 'Stock Status';
-$_['text_button_apply'] = 'Apply';
-$_['text_reset_all'] = 'Reset All';
+$_['text_button_apply'] = 'Применить';
+$_['text_reset_all'] = 'Сбросить все';
 $_['text_show_more'] = 'Show more (%s)';
 $_['text_show_less'] = 'Show less';
 $_['text_display'] = 'Display';
